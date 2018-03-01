e7b4933d3bb0d3b4cc5c77d9c20098ec8a677c0a
Marta <martadipuccio@me.com>
2018-03-01 16:02:21 +0100

    code implemented with google maps,on click from flag, dinamically created

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ function getCountries(){
         countriesArray = countries;
         createLayout(countriesArray);
     })
-    .catch(err => {
+    /* .catch(err => {
        alert('Fetch Error :-S', err);
-    });     
+    });   */   
     
 }
 
@@ -26,25 +26,26 @@ function createLayout(countriesArray) {
     for(var i = 0; i < countriesArray.length; i++){
         worldPopulation += countriesArray[i].population
     };
-    //console.log(countriesArray)
     
-    //sort population descendent and ascendent
+    
+    //sort population descendant and ascendant
     if (window.location.search.substr(1) === "sort=asc"){
         countriesArray.sort((a, b) => b.population - a.population)
     } else if (window.location.search.substr(1) === "sort=desc"){
         countriesArray.sort((a, b) => a.population - b.population)
     }
     
-    htmlElements += `<th>Country</th><th>Capital</th><th>Language</th><th>Currency</th><th>Population<br><a href="index.html?sort=asc"><img src="https://png.icons8.com/windows/20/000000/chevron-up.png"></a><a href="index.html?sort=desc"><img src="https://png.icons8.com/windows/20/000000/chevron-down.png"></a></th><th>World Population <img src="https://png.icons8.com/material/20/000000/percentage.png"></th><th>Borders</th>`;
+    htmlElements += `<th>Country</th><th>Flags</th><th>Capital</th><th>Language</th><th>Currency</th><th>Population<br><a href="index.html?sort=asc"><img src="https://png.icons8.com/windows/20/000000/chevron-up.png"></a><a href="index.html?sort=desc"><img src="https://png.icons8.com/windows/20/000000/chevron-down.png"></a></th><th>World Population <img src="https://png.icons8.com/material/20/000000/percentage.png"></th><th>Borders</th>`;
     singleCountry.forEach(country => {
-        htmlElements += `<tr><td><img src = "${country.flag}" width="50" />${country.name}</td>`;
-        htmlElements += `<td>${country.capital}</td>`;
-        htmlElements += `<td>${country.languages[0].name}</td>`;
-        htmlElements += `<td>${country.currencies[0].name}</td>`;
-        htmlElements += `<td>${country.population}</td>`;
-        htmlElements += `<td>${(country.population / worldPopulation * 100).toFixed(3)}%</td>`;
-        htmlElements += `<td>${country.borders}</td></tr>`;
-        
+        htmlElements += `<tbody><tr>
+        <td>${country.name}</td>
+        <td  id="tableMap" ><img data-id="${country.latlng}" src = "${country.flag}" width="50" /></td>
+        <td>${country.capital}</td>
+        <td>${country.languages[0].name}</td>
+        <td>${country.currencies[0].name}</td>
+        <td>${country.population}</td>
+        <td>${(country.population / worldPopulation * 100).toFixed(3)}%</td>
+        <td>${country.borders}</td></tr></tbody>`;
         
     });
     
@@ -52,8 +53,50 @@ function createLayout(countriesArray) {
     htmlElements += `</div></table>`;
     div.innerHTML = htmlElements;
     
+    const showingMap = document.querySelectorAll('#tableMap');
+    showingMap.forEach(country => {
+        country.addEventListener('click', (e) => {
+            
+            // const {target} = e; = const target = e.target
+            const coordinates = e.target.getAttribute('data-id');
+            //console.log(coordinates)
+            const final = coordinates.split(",");
+            initMap(Number(final[0]),Number(final[1]), 6);
+            $('.bd-example-modal-lg').modal()
+        })
+    })
     
-};
+    
+}
+
+
+
+
+function initMap(lat, lng, zoom) {
+    const coordinates = {
+        lat:lat,
+        lng:lng 
+    }
+    const map = new google.maps.Map(document.getElementById('map'), {
+        zoom:zoom, 
+        center:coordinates
+    })
+    const marker = new google.maps.Marker({
+        position:coordinates,
+        map:map
+    })
+    const panorama = new google.maps.StreetViewPanorama(
+        document.getElementById('pano'), {
+          position: coordinates,
+          pov: {
+            heading: 34,
+            pitch: 10
+          }
+        });
+    map.setStreetView(panorama);
+}
+
+
 
 
 function searchCountry(){    
@@ -65,11 +108,11 @@ function searchCountry(){
         
         
     })
-};
+}
 
 
 
 getCountries();
 searchCountry();
-
+//initMap(-27, 133, 5);
 
